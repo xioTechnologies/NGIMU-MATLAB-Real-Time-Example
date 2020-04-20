b803e059feac2f3ee364589bb0eda9423c14fe3c
x-io Technologies <social@x-io.co.uk>
2020-04-20 13:47:05 +0100

    Remove dependency on quat2rotm

--- quaternionPlot.m
+++ quaternionPlot.m
@@ -54,7 +54,18 @@ classdef quaternionPlot < handle
         function obj = updatePlot(obj)
 
             % Convert quaternion to rotation matrix
-            R = quat2rotm(obj.quaternion);
+            %R = quat2rotm(obj.quaternion);
+            q = obj.quaternion;
+            R = eye(3);
+            R(1,1) = 2.*q(1).^2-1+2.*q(2).^2;
+            R(1,2) = 2.*(q(2).*q(3)+q(1).*q(4));
+            R(1,3) = 2.*(q(2).*q(4)-q(1).*q(3));
+            R(2,1) = 2.*(q(2).*q(3)-q(1).*q(4));
+            R(2,2) = 2.*q(1).^2-1+2.*q(3).^2;
+            R(2,3) = 2.*(q(3).*q(4)+q(1).*q(2));
+            R(3,1) = 2.*(q(2).*q(4)+q(1).*q(3));
+            R(3,2) = 2.*(q(3).*q(4)-q(1).*q(2));
+            R(3,3) = 2.*q(1).^2-1+2.*q(4).^2;
 
             % Udate teapot
             rotatedTeaPot = pctransform(obj.teaPot, affine3d(rotm2tform(R)));
